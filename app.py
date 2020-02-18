2ff0caca6ece81d43cfc951d75960c2464593b60
Yifei Sun <yifei-sun-1@uiowa.edu>
2020-02-19 05:19:45 +0800

    Basic feature created/ Wait to show variable in the website

--- app.py
+++ app.py
@@ -2,9 +2,25 @@ from flask import Flask
 from flask import render_template, request
 app = Flask(__name__)
 
-@app.route('/')
-def index():    
-    return render_template('index.html',pageTitle='Calculator main page')
+@app.route('/',methods=['GET','POST'])
+def index(): 
+        if request.method=='POST':
+            form=request.form
+            Loan_amount=float(form['loan_amount'])
+            Payments_per_year=float(form['payment_per_year'])
+            Number_of_year=float(form['number_of_year'])
+            Annual_rate=float(form['annual_rate'])
+            Payment_period=float(form['payment_period'])
+
+            n=Payments_per_year*Number_of_year
+            i=Annual_rate/Payment_period
+            D=((1+i)**n-1)/(i*( 1+ i)**n)
+            P=Loan_amount/D
+
+            print(f'Your loan payment is {P}$')
+    
+        return render_template('index.html',pageTitle='Calculator main page')
+
 
 if __name__ == '__main__':   
      app.run(debug=True, host='0.0.0.0')
